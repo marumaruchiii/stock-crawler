31c087d4e81011034f8c76879cca90f68a7300c4
Noel Ho <ksan0914@gmail.com>
2023-10-20 21:15:02 +0800

    comment update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from time import sleep
 twse_url = "https://www.twse.com.tw/en/exchangeReport/MI_INDEX?response=csv&type=ALLBUT0999&date=20231013"
 tpex_url = "https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=en-us&o=csv&se=EW&s=0,asc,0&d=20231013"
 
-
+#Fu
 def get_exchange_report(url):
     report = requests.get(url)
     sleep(10)
@@ -20,3 +20,4 @@ print("### TPEX REPORT ###")
 tpex_report = get_exchange_report(tpex_url).splitlines()
 for line in tpex_report:
     print(line)
+
