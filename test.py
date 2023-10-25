db44e7a4b4aaa7fc4cf33a2d106f94550df59b7a
Noel Ho <ksan0914@gmail.com>
2023-10-25 21:57:35 +0800

    import bokeh

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import requests
+import numpy as np
+from bokeh.plotting import figure, show
 from time import sleep
+
 
 twse_url = "https://www.twse.com.tw/en/exchangeReport/MI_INDEX?response=csv&type=ALLBUT0999&date=20231013"
 tpex_url = "https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=en-us&o=csv&se=EW&s=0,asc,0&d=20231013"
@@ -21,3 +24,21 @@ tpex_report = get_exchange_report(tpex_url).splitlines()
 for line in tpex_report:
     print(line)
 
+
+
+
+#graphic
+x = np.arange(0, 10, 1)
+y1 = x **2
+y2 = x **3
+y3 = x **4
+
+
+p = figure (title="Simple Line Charts", x_axis_label="x", y_axis_label="y")
+
+
+p.line(x, y1, legend_label="", line_width=2, color="red")
+p.line(x, y2, legend_label="", line_width=2, color="green")
+p.line(x, y3, legend_label="", line_width=2, color="blue")
+
+show(p)
